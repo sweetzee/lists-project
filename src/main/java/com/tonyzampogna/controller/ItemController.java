b92ff42eec9213a504de656cd9496b01e5b04b63
Tony Zampogna <tony.zampogna@target.com>
2016-12-08 09:23:35 -0600

    add permissions to item controller.

--- src/main/java/com/tonyzampogna/controller/ItemController.java
+++ src/main/java/com/tonyzampogna/controller/ItemController.java
@@ -1,6 +1,8 @@
 package com.tonyzampogna.controller;
 
 import com.tonyzampogna.domain.ItemModel;
+import com.tonyzampogna.domain.UserListModel;
+import com.tonyzampogna.domain.UserModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -114,7 +116,7 @@ public class ItemController extends BaseController {
 
 
 	/////////////////////////////////////////////////
-	// Helper Methods
+	// Handler Methods
 	/////////////////////////////////////////////////
 
 	private List<ItemModel> handleCreateItemsRequest(String createUserId, List<ItemModel> itemModels) {
@@ -173,4 +175,142 @@ public class ItemController extends BaseController {
 
 		return itemModels;
 	}
+
+
+	/////////////////////////////////////////////////
+	// Permission Validation Methods
+	/////////////////////////////////////////////////
+
+	private void validateCreateItemPermissions(String requestUserId, List<ItemModel> itemModels) {
+		UserModel requestUserModel = getUserModel(requestUserId);
+		if (requestUserModel == null) {
+			throw new RuntimeException("The user requesting the operation does not exist. Request User ID: " + requestUserId);
+		}
+
+		for (ItemModel itemModel : itemModels) {
+			boolean canCreateItem = false;
+			UUID userId = requestUserModel.getUserId();
+			UUID listId = itemModel.getListId();
+
+			// Admins can create items.
+			if (UserModel.AuthorizationLevel.ADMIN.equals(requestUserModel.getAuthorizationLevel())) {
+				canCreateItem = true;
+			}
+
+			// Check the permission on the list.
+			UserListModel userListModel = getUserListModel(userId, listId);
+			boolean hasWritePermission = listService.hasWritePermission(userId, userListModel);
+			if (hasWritePermission) {
+				canCreateItem = true;
+			}
+
+			if (!canCreateItem) {
+				throw new RuntimeException("The requesting user cannot create the item. Request User ID: " + requestUserId + ". Item ID: " + itemModel.getItemId());
+			}
+		}
+	}
+
+	private void validateReadItemPermissions(String requestUserId, List<ItemModel> itemModels) {
+		UserModel requestUserModel = getUserModel(requestUserId);
+		if (requestUserModel == null) {
+			throw new RuntimeException("The user requesting the operation does not exist. Request User ID: " + requestUserId);
+		}
+
+		for (ItemModel itemModel : itemModels) {
+			boolean canReadItem = false;
+			UUID userId = requestUserModel.getUserId();
+			UUID listId = itemModel.getListId();
+
+			// Admins can create lists.
+			if (UserModel.AuthorizationLevel.ADMIN.equals(requestUserModel.getAuthorizationLevel())) {
+				canReadItem = true;
+			}
+
+			// Check the permission on the list.
+			UserListModel userListModel = getUserListModel(userId, listId);
+			boolean hasReadPermission = listService.hasReadPermission(userId, userListModel);
+			if (hasReadPermission) {
+				canReadItem = true;
+			}
+
+			if (!canReadItem) {
+				throw new RuntimeException("The requesting user cannot read the item. Request User ID: " + requestUserId + ". Item ID: " + itemModel.getItemId());
+			}
+		}
+	}
+
+	private void validateUpdateItemPermissions(String requestUserId, List<ItemModel> itemModels) {
+		UserModel requestUserModel = getUserModel(requestUserId);
+		if (requestUserModel == null) {
+			throw new RuntimeException("The user requesting the operation does not exist. Request User ID: " + requestUserId);
+		}
+
+		for (ItemModel itemModel : itemModels) {
+			boolean canUpdateItem = false;
+			UUID userId = requestUserModel.getUserId();
+			UUID listId = itemModel.getListId();
+
+			// Admins can create lists.
+			if (UserModel.AuthorizationLevel.ADMIN.equals(requestUserModel.getAuthorizationLevel())) {
+				canUpdateItem = true;
+			}
+
+			// Check the permission on the list.
+			UserListModel userListModel = getUserListModel(userId, listId);
+			boolean hasWritePermission = listService.hasWritePermission(userId, userListModel);
+			if (hasWritePermission) {
+				canUpdateItem = true;
+			}
+
+			if (!canUpdateItem) {
+				throw new RuntimeException("The requesting user cannot read the item. Request User ID: " + requestUserId + ". Item ID: " + itemModel.getItemId());
+			}
+		}
+	}
+
+	private void validateDeleteItemPermissions(String requestUserId, List<ItemModel> itemModels) {
+		UserModel requestUserModel = getUserModel(requestUserId);
+		if (requestUserModel == null) {
+			throw new RuntimeException("The user requesting the operation does not exist. Request User ID: " + requestUserId);
+		}
+
+		for (ItemModel itemModel : itemModels) {
+			boolean canDeleteItem = false;
+			UUID userId = requestUserModel.getUserId();
+			UUID listId = itemModel.getListId();
+
+			// Admins can create lists.
+			if (UserModel.AuthorizationLevel.ADMIN.equals(requestUserModel.getAuthorizationLevel())) {
+				canDeleteItem = true;
+			}
+
+			// Check the permission on the list.
+			UserListModel userListModel = getUserListModel(userId, listId);
+			boolean hasWritePermission = listService.hasWritePermission(userId, userListModel);
+			if (hasWritePermission) {
+				canDeleteItem = true;
+			}
+
+			if (!canDeleteItem) {
+				throw new RuntimeException("The requesting user cannot read the item. Request User ID: " + requestUserId + ". Item ID: " + itemModel.getItemId());
+			}
+		}
+	}
+
+
+	/////////////////////////////////////////////////
+	// Helper Methods
+	/////////////////////////////////////////////////
+
+	private UserListModel getUserListModel(UUID userId, UUID listId) {
+		List<UserListModel> userListModels = listService.getUserListsByUserId(userId);
+		for (UserListModel userListModel : userListModels) {
+			if (userListModel.getUserId().equals(userId) &&
+				userListModel.getListId().equals(listId)) {
+				return userListModel;
+			}
+		}
+		return null;
+	}
+
 }
